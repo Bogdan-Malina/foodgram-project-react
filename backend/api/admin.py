01e37f548344f73f3429fd9d7031a05021c23dd9
Данил <habo3hnk1337@gmail.com>
2022-11-21 22:19:54 +0300

    база

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import ShoppingList, Favorite, Ingredient, Recipes, Tag
 
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+
+class IngredientResource(resources.ModelResource):
+   class Meta:
+      model = Ingredient
+
 
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('name', 'author', 'count')
@@ -10,7 +18,8 @@ class RecipeAdmin(admin.ModelAdmin):
         return obj.favorites.count()
 
 
-class IngredientAdmin(admin.ModelAdmin):
+class IngredientAdmin(ImportExportModelAdmin):
+    resource_class = IngredientResource
     list_display = ('name', 'measurement_unit')
     list_filter = ('name',)
 
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'import_export',
     'colorfield',
     'rest_framework',
     'django_filters',
